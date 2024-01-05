fcb94a19cc6aacd331c49ea5c904d88811433f7e
meedotakou <mohamed.otakoucht@edu.uiz.ac.ma>
2024-01-05 16:07:06 +0100

    init commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-          "googleusercontent.com",
-          "oaidalleapiprodscus.blob.core.windows.net",
-          "cdn.openai.com"
-        ]
+  images: {
+    domains: [
+      "googleusercontent.com",
+      "oaidalleapiprodscus.blob.core.windows.net",
+      "cdn.openai.com"
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/logo.png',
+        headers: [
+          {
+            key: 'Link',
+            value: '</logo.png>; rel="icon"',
+          },
+        ],
       },
+    ];
+  },
 }
 
 module.exports = nextConfig
